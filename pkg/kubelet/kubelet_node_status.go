ff40d99751013a55c20d547a18d714c485e4e131
Shelley-BaoYue <baoyue2@huawei.com>
2022-10-25 19:47:25 +0800

    modify KubeletListenServer for logs/exec feature in KubeEdge
    Signed-off-by: Shelley-BaoYue <baoyue2@huawei.com>

--- pkg/kubelet/kubelet_node_status.go
+++ pkg/kubelet/kubelet_node_status.go
@@ -355,6 +355,7 @@ func (kl *Kubelet) initialNode(ctx context.Context) (*v1.Node, error) {
 	}
 
 	kl.setNodeStatus(ctx, node)
+	node.Status.DaemonEndpoints = *kl.daemonEndpoints
 
 	return node, nil
 }
@@ -660,7 +661,6 @@ func (kl *Kubelet) defaultNodeStatusFuncs() []func(context.Context, *v1.Node) er
 		nodestatus.MachineInfo(string(kl.nodeName), kl.maxPods, kl.podsPerCore, kl.GetCachedMachineInfo, kl.containerManager.GetCapacity,
 			kl.containerManager.GetDevicePluginResourceCapacity, kl.containerManager.GetNodeAllocatableReservation, kl.recordEvent, kl.supportLocalStorageCapacityIsolation()),
 		nodestatus.VersionInfo(kl.cadvisor.VersionInfo, kl.containerRuntime.Type, kl.containerRuntime.Version),
-		nodestatus.DaemonEndpoints(kl.daemonEndpoints),
 		nodestatus.Images(kl.nodeStatusMaxImages, kl.imageManager.GetImageList),
 		nodestatus.GoRuntime(),
 	)
